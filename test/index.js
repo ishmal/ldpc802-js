d9b6db4f97314aaeb41e077a1e3543935755f10a
ishmal <ishmal@github.org>
2018-10-14 18:22:53 -0500

    add index and update

---
+++ test/index.js
@@ -0,0 +1,10 @@
+
+const LdpcDecoder = require("./src/ldpcDecoder");
+const LdpcEncoder = require("./src/LdpcEncoder");
+const CodeTables = require("./src/codeTables");
+
+module.exports = {
+	LdpcDecoder,
+	LdpcEncoder,
+	CodeTables
+};
